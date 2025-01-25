cf8cf24b8c0586b8ab89e313c9a9dab1b5ba7fea
antonio-moya-aleba <moyacalvo.cursos@gmail.com>
2025-01-25 13:12:06 +0100

    Inicio de proyecto

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="../public/favicon.ico" width="125" height="125" />
+  </header>
+  <main>
+    <h1>Bienvenido a mi Proyecto Vue</h1>
+  </main>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  line-height: 1.5;
+  background-color: var(--color-background);
+  display: flex;
+ flex-direction: row;
+ justify-content: center;
+}
+img{
+  margin: auto;
+}
+
+h1{
+  text-align: center;
+  margin: 1em 0;
+}
+
+.logo {
+  background-color: var(--color-background);
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+}
+</style>
