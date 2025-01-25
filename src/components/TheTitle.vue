c3f7a866869168ad7d5000bc1ca30fa4a0aae6c7
Antonio <moyacalvo.cursos@gmail.com>
2025-01-25 20:20:31 +0100

    feature(components): Create my first component

---
+++ src/components/TheTitle.vue
@@ -0,0 +1,10 @@
+<template>
+    <h2>Por fin un proyecto Vuejs</h2>
+</template>
+<style scoped>
+    h2{
+        margin: auto;
+        text-align: center;
+        color: var(--color-text-secondary);
+    }
+</style>
