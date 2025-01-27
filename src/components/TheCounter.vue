7ba2fc4bd74c62360791b3e6d94696e692cbd3c9
Antonio <antonio.moya@alebateducation.com>
2025-01-27 12:14:49 +0100

    instalado sass-embedded

--- src/components/TheCounter.vue
+++ src/components/TheCounter.vue
@@ -1,45 +1,65 @@
 <script setup>
-import { ref } from 'vue'
-import TheTitle from './TheTitle.vue'
+import { ref } from "vue";
+import TheTitle from "./TheTitle.vue";
 
-const counter = ref(0)
+const counter = ref(1);
 
-function increment() { counter.value++ }
-function decrement() { counter.value-- }
-
+function increment() {
+  counter.value++;
+}
+function decrement() {
+  counter.value--;
+}
 </script>
 
 <template>
-<section>
-<TheTitle>Este contador puede cambiar su valor de forma dinámica </TheTitle>
-<div class="contador">
-    <p>El contador tiene un valor de: <span>{{ counter }}</span> </p>
-    <p><button v-if="(counter < 10)" @click="increment">Incrementar</button> o <button v-if="(counter > 0)" @click="decrement">Decrementar</button></p>
-    <TheTitle v-if="(counter >= 10 || counter <= 0)">HAS LLEGADO AL LÍMITE</TheTitle>
-</div>
-</section>
+  <section class="section">
+    <TheTitle class="section__title"
+      >Este contador puede cambiar su valor de forma dinámica</TheTitle
+    >
+    <div class="section__contador">
+      <p>
+        El contador tiene un valor de:
+        <span class="section__contador__value">{{ counter }}</span>
+      </p>
+      <p>
+        <button v-if="counter < 10" @click="increment">Incrementar</button> o
+        <button v-if="counter > 0" @click="decrement">Decrementar</button>
+      </p>
+      <TheTitle
+        v-if="counter >= 10 || counter <= 0"
+        class="section__contador__limit"
+      >
+        HAS LLEGADO AL LÍMITE
+      </TheTitle>
+    </div>
+  </section>
 </template>
 
-<style>
-    section {
-        width:fit-content;
-        margin: auto;
-        padding: 1em 1em;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    .contador{
-        margin: 1em 0;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 1em;
-    }
-    span{
-        font-size: 2em;
-        color: var(--color-text-secondary);
-    }
+<style lang="scss" scoped>
+.section {
+  width: fit-content;
+  margin: auto;
+  padding: 1em 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
+.section__contador {
+  margin: 1em 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1em;
 
+  &__value {
+    font-size: 2em;
+    color: var(--color-text-secondary);
+  }
+
+  &__limit {
+    font-weight: bold;
+  }
+}
 </style>
